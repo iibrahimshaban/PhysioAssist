4d779be40f9e66a4b7780b9ebf02dbe66080ebe3
kareemz99 <kemoaltayeb979@gmail.com>
2026-07-02 06:16:09 +0300

    added patient repo , DTOs

---
+++ PhysioAssist.Api/Modules/PatientModule/Repositories/IPatientRepo.cs
@@ -0,0 +1,15 @@
+﻿using PhysioAssist.Api.Modules.PatientModule.Entities;
+using PhysioAssist.Api.Shared.Interfaces;
+
+namespace PhysioAssist.Api.Modules.PatientModule.Repositories
+{
+    public interface IPatientRepo:IBaseRepository<Patient>
+    {
+        //Task<Patient?> GetByIdAsync(Guid patientId);
+        //Task UpdatePatientAsync(Patient patient);
+        //Task DeletePatientAsync(Patient patient);
+        //Task AddPatientAsync(Patient patient);
+
+        //IQueryable<Patient> GetPatients_Ordered();
+    }
+}
---
+++ PhysioAssist.Api/Modules/PatientModule/Repositories/PatientRepo.cs
@@ -0,0 +1,45 @@
+﻿using PhysioAssist.Api.Modules.PatientModule.Entities;
+using PhysioAssist.Api.Persistence;
+
+namespace PhysioAssist.Api.Modules.PatientModule.Repositories
+{
+    public class PatientRepo : IPatientRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PatientRepo(ApplicationDbContext context) 
+        {
+            _context = context; 
+        }
+        
+        public async Task AddAsync(Patient entity)
+        {
+            await _context.Patients.AddAsync(entity);
+        }
+
+        public void Delete(Patient entity)
+        {
+            _context.Patients.Remove(entity);
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllAsync()
+        {
+            return await _context.Patients.ToListAsync();
+        }
+
+        public async Task<Patient?> GetByIdAsync(int id)
+        {
+            return await _context.Patients.FindAsync(id);
+        }
+
+        //public IQueryable<Patient> GetPatients_Ordered()
+        //{
+        //    _context.Patients.OrderBy(p => p.FullName);
+        //}
+
+        public void Update(Patient entity)
+        {
+            _context.Patients.Update(entity);
+        }
+    }
+}
